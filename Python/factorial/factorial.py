7656e6c72458907cdb48b80c021db6eb774d1489
richard.moulton <richard.moulton>
2021-06-17 19:38:23 -0400

    Initial commit

---
+++ Python/factorial/factorial.py
@@ -0,0 +1,13 @@
+# This is an implementation of the factorial function
+
+def factorial():
+    pass
+
+
+def main():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
