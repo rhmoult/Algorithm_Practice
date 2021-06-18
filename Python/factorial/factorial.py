d9d24d80738c8b67e358e1706ad6feeb36957ecf
rhmoult <rhmoult@gmail.com>
2021-06-17 20:40:41 -0400

    Implemented factorial function

--- Python/factorial/factorial.py
+++ Python/factorial/factorial.py
@@ -1,11 +1,14 @@
 # This is an implementation of the factorial function
 
-def factorial():
-    pass
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 
 def main():
-    pass
+    print(factorial(5))
 
 
 # Press the green button in the gutter to run the script.
