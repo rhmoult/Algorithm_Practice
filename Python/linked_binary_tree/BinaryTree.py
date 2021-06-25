80092178e55de104e417a7167a0fc45928d94160
Rich Moulton <rhmoult@gmail.com>
2021-06-25 19:30:11 -0400

    Completed implementation

--- Python/linked_binary_tree/BinaryTree.py
+++ Python/linked_binary_tree/BinaryTree.py
@@ -16,3 +16,22 @@ class BinaryTree(Tree):
         Return None if p does not have a right child.
         """
         raise NotImplementedError('must be implemented by subclass')
+
+    # -------------------- additional abstract methods -------------
+    def sibling(self, p):
+        """Return a Position representing p's sibling (or None if no sibling)"""
+        parent = self.parent(p)
+        if parent is None:                  # p must be the root
+            return None                     # root has no sibling
+        else:
+            if p == self.left(parent):
+                return self.right(parent)   # possibly None
+            else:
+                return self.left(parent)    # possibly None
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children."""
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
