cdc717defecc2deab6f9a264eaa54551e75a6c0c
rhmoult <rhmoult@gmail.com>
2021-06-26 04:31:14 -0400

    Make PEP 8 compat and initial push of p-queue

---
+++ Python/rhmalgorithmic/priority_queue/unsorted_priority_queue.py
@@ -0,0 +1,36 @@
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """A min-oriented priority queue implemented with an unsorted list."""
+
+    def _find_min(self):    # nonpublic utility
+        """Return Position of item with minimum key."""
+        if self.is_empty():
+            raise Empty('Priority queue is empty')
+        small = self._data.first()
+        while walk is not None:
+            if walk.element() < small.element():
+                small = walk
+            walk = self._data.after(walk)
+        return small
+
+    def __init__(self):
+        """Create a new empty Priority Queue."""
+        self._data = PositionalList()
+
+    def __len__(self):
+        """Return the number of items in the priority queue."""
+        return len(self._data)
+
+    def add(self, key, value):
+        self._data.add_last(self._Item(key, value))
+
+    def min(self):
+        """Return but do not remove (k,v) tuple with minimum key."""
+        p = self._find_min()
+        item = p.element()
+        return (item._key, item._value)
+
+    def remove_min(self):
+        """Remove and return (k,v) tuple with minimum key."""
+        p = self._find_min()
+        item = self.data.delete(p)
+        return (item._key, item._value)
