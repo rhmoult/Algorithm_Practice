26cf028f34d1c5266fffb632d9275ea8ddbf32f1
rhmoult <rhmoult@gmail.com>
2021-07-03 21:33:01 -0400

    Make algorithm take less time

--- Python/extra/fibonacci/fibonacci_last_digit.py
+++ Python/extra/fibonacci/fibonacci_last_digit.py
@@ -4,15 +4,13 @@ def get_fibonacci(num, fibonacci):
         return num
     else:
         for i in range(2,num+1):
-            fibonacci[2] = fibonacci[1] + fibonacci[0]
-            fibonacci[0] = fibonacci[1]
-            fibonacci[1] = fibonacci[2]
-    return fibonacci[2]
+            fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 10
+    return fibonacci[num]
 
 def main():
     n = int(input())
-    starter_hash = {0:0,1:1,2:1}
-    print(str(get_fibonacci(n, starter_hash))[-1])
+    starter_array = list(range(0,n+1))
+    print(str(get_fibonacci(n, starter_array))[-1])
 
 
 if __name__ == '__main__':
