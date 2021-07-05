c1922a7b56224d9ee2b16cea2b87cf5ecbaa8722
Rich Moulton <rhmoult@gmail.com>
2021-07-05 19:42:39 -0400

    Initial commit

---
+++ Python/extra/fibonacci/fibonacci_huge.py
@@ -0,0 +1,25 @@
+# Uses python3
+import sys
+
+
+def get_fibonacci_huge_mod(n, m):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current % m
+
+
+def main():
+    input = sys.stdin.read();
+    n, m = map(int, input.split())
+    print(get_fibonacci_huge_mod(n, m))
+
+
+if __name__ == '__main__':
+    main()
