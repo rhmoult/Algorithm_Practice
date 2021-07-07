6b4b98255e3fcc7d305a6b6455003e005a8bf75b
Rich Moulton <rhmoult@gmail.com>
2021-07-06 21:34:40 -0400

    First stab

---
+++ Python/extra/fibonacci/fibonacci_sum_last_digit.py
@@ -0,0 +1,20 @@
+# Uses python3
+import sys
+
+def fibonacci_sum_fast(num):
+    if num < 2:
+        return num
+    else:
+        previous = 0
+        current  = 1
+        sum      = 1
+        for i in range(2,num+1):
+            previous, current = current, (current + previous) % 10
+            sum += current
+
+    return sum % 10
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    print(fibonacci_sum_fast(n))
